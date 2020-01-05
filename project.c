e9c82b9da80af454985edb3d03408ffe9bd559bb
reyhanehtt <reyhaneh.tarighatytabesh@gmail.com>
2020-01-05 22:54:42 +0330

    avvalie

---
+++ project.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct Film
+{
+    int movieLen;
+    char act[10][20] ;
+};
+struct Saans
+{
+    struct Film film ;
+    int capacity ;
+    int start , ending ;
+};
+struct Salons
+{
+    struct Saans saans[10];
+};
+struct Cinema
+{
+    struct Salons salons[10] ;
+};
