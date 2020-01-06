6b37c79034eee2f62aa7cb9edb905a6e712c2d3b
reyhanehtt <reyhaneh.tarighatytabesh@gmail.com>
2020-01-06 20:25:44 +0330

    avvalie

--- project.c
+++ project.c
@@ -1,5 +1,51 @@
 #include<stdio.h>
 
+//sort algorithm
+
+void swap (int *a , int *b)
+{
+    int temp= *a ;
+    *a = *b ;
+    *b = temp ;
+}
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1) , j;
+
+    for (j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+int partition_r(int arr[], int low, int high)
+{
+    srand(time(NULL));
+    int random = low + rand() % (high - low);
+    swap(arr[random], arr[high]);
+
+    return partition(arr, low, high);
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high) {
+        int pi = partition_r(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+//structures
+
 struct Film
 {
     int movieLen;
